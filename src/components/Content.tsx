395879c24bdcb5fba1a8a700019ad8f09f7cc454
Camila <camilaminei.ifms@gmail.com>
2021-11-10 23:13:02 -0400

    desafio 2 finalizado

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,34 @@
-export function Content() {
-  // Complete aqui
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../components/MovieCard';
+import { MovieProps } from '../App';
+import { GenreResponseProps } from '../components/SideBar';
+
+
+interface ContentProps {
+    movies: MovieProps[];
+    selectedGenre: GenreResponseProps;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+
+    return (
+        <div className="container">
+            <header>
+                <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+            </header>
+
+            <main>
+                <div className="movies-list">
+                    {movies.map(movie => (
+                        <MovieCard key={movie.imdbID}
+                            title={movie.Title}
+                            poster={movie.Poster}
+                            runtime={movie.Runtime}
+                            rating={movie.Ratings[0].Value} />
+                    ))}
+                </div>
+            </main>
+        </div>
+    )
+
 }
